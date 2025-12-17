28dfc9c58a6ec06cd6e403b9cb73e5a34857b565
kayumkadivar <kayumkadivar786@gmail.com>
2025-12-17 10:16:24 +0530

    Implement initial project structure and setup

---
+++ frontend/src/App.jsx
@@ -0,0 +1,11 @@
+import React from "react";
+
+const App = () => {
+  return (
+    <div className="bg-slate-50 h-screen">
+      <h1 className="text-3xl font-bold text-blue-500">App</h1>
+    </div>
+  );
+};
+
+export default App;
